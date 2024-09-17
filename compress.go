48a6cee4b72ce4c2dd1c5e5c86dbcf118b556c57
dna737 <adnanhere27@gmail.com>
2024-09-17 09:51:46 -0500

    ip: read bytes; (use a map to get specifics)

--- compress.go
+++ compress.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func countChars(f *os.File) {
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanBytes)
+	arr := []int{}
+
+	for scanner.Scan() {
+		// append(arr, )
+	}
+}
 
 func main() {
-	fmt.Println("test")
+
+	if len(os.Args[1:]) != 1 {
+		fmt.Println("Please provide a valid path to the file name as an option.")
+		os.Exit(1)
+	}
+	
+	filename := os.Args[1]
+	f, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println("Please provide a valid path to the file name as an option.")
+	}
+	
+	countChars(f)
+	
 }
