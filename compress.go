99cea009c299a683c11a1af4d260ef87e12d52e0
dna737 <adnanhere27@gmail.com>
2024-09-19 19:51:10 -0500

    ip: gbuildTree(): start combining nodes

--- compress.go
+++ compress.go
@@ -7,10 +7,11 @@ import (
 	"unicode"
 )
 
-type HuffTree struct {
+type HuffNode struct {
 	weight int
 	left string
 	right string
+	isLeaf bool
 }
 
 func countChars(f *os.File) map[string]int {
@@ -32,23 +33,52 @@ func countChars(f *os.File) map[string]int {
 	return occ
 }
 
-func compGen(occ map[string]int) []string {
+func compGen(occ map[string]int) ([]string, []HuffNode) {
 	occCopy := occ
 	var result []string
+	var nodes []HuffNode
 
 	for len(occCopy) > 0 {
-		_, k1, k2 := generateNodes(occCopy)
+		n, k1, k2 := generateNodes(occCopy)
 		fmt.Println(k1, k2)
 		delete(occCopy, k1)
 		delete(occCopy, k2)
 
 		result = append(result, k1, k2)
+		nodes = append(nodes, n)
 	}
 
-	return result
+	return result, nodes
 }
 
-func generateNodes(occ map[string]int) (HuffTree, string, string) {
+func buildTree(nodes []HuffNode) HuffNode {
+	var result HuffNode
+
+	//Nodes are in increasing order of freq, and will form pairs first.
+	for len(nodes) != 1 {
+		for i, n := range nodes {
+			var secondVal int
+
+			if i + 1 == len(nodes) {
+				//No second node
+				secondVal = 0
+			} else {
+				secondVal = nodes[i + 1].weight
+			}
+
+			newNode := HuffNode{
+				weight: n.weight + secondVal,
+				left: "",
+				right: "",
+				isLeaf: false,
+			}
+
+			// nodes
+		}
+	}
+}
+
+func generateNodes(occ map[string]int) (HuffNode, string, string) {
 
 	smallest, secondSmallest := 0, 0
 	var smallestChar, secondSmallestChar string
@@ -82,10 +112,11 @@ func generateNodes(occ map[string]int) (HuffTree, string, string) {
 		}
 	}
 
-	a := HuffTree{
+	a := HuffNode{
 		weight: (smallest + secondSmallest),
 		left:   smallestChar,
 		right:  secondSmallestChar,
+		isLeaf: true,
 	}
 
 	fmt.Println(a)
@@ -102,5 +133,4 @@ func main(){
 	}
 	
 	countChars(f)
-
 }
